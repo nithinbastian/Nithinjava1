3ffb34338940efc7c31cdb8c9fbed28d6dc6f84c
USER <USER@USER-PC>
2022-08-10 08:11:04 +0530

    initial commit

---
+++ test/src/my/lesson1.java
@@ -0,0 +1,19 @@
+package my;
+
+public class lesson1 {
+	public void display () {
+		System.out.println("hello");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n1=12345;
+		int n2=4321;
+   String s="hello";
+   String s1=new String("welcome");
+   String s2=s+n1;
+   System.out.println(s2);
+   int n3=n2+n1;
+   System.out.println(n3);
+	}
+}
---
+++ test/src/test/SecondClass.java
@@ -0,0 +1,11 @@
+package test;
+
+public class SecondClass {
+String isEvenOrOdd(int value)
+{
+	if(value%2==0)
+		return "Even";
+	return "odd";
+}
+
+}
